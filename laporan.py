7a81976308e21aaad264ab5757255e4b7e1a76fe
Farhan Zein <farzein27@gmail.com>
2022-11-30 15:57:13 +0700

    update

--- laporan.py
+++ laporan.py
@@ -4,6 +4,10 @@ df = pd.read_csv('bijikopi.csv')
 df = df.astype({'bulan':str, 'tahun':str})
 
 products = df['produk'].unique()
+prod_dict = {
+'0':'arabika toraja','1':'arabika blue ijen','2':'arabika gayo','3':'arabika kintamani',
+'4':'robusta bogor','5':'robusta gayo','6':'arabika papua wamena','7':'robusta dampit'
+}
 def show_laporan(bulan, tahun):
   if df[(df['bulan'] == bulan) & (df['tahun'] == tahun)].empty:
     print(f'Laporan Penjualan Bulan {bulan} Tahun {tahun} Tidak Ditemukan')
@@ -14,3 +18,84 @@ def show_laporan(bulan, tahun):
     for i in products:
       print(f'total penjualan produk {i} \t\t:', df[(df['bulan'] == bulan) & (df['tahun'] == tahun) & (df['produk'] == i)]['harga_produk'].count())
       print(f'total pendapatan produk {i} \t\t:', df[(df['bulan'] == bulan) & (df['tahun'] == tahun) & (df['produk'] == i)]['harga_produk'].sum())
+
+def add_data(bulan, tahun, produk, harga):
+    new_data = {'bulan':bulan, 'tahun':tahun, 'produk':prod_dict[produk], 'harga_produk':harga}
+    global df
+    df = df.append(new_data, ignore_index = True)
+    df.to_csv('bijikopi.csv', index=False)
+
+def show_data(bulan, tahun):
+  if df[(df['bulan'] == bulan) & (df['tahun'] == tahun)].empty:
+    print(f'Laporan Penjualan Bulan {bulan} Tahun {tahun} Tidak Ditemukan')
+  else:
+    print(df[(df['bulan'] == bulan) & (df['tahun'] == tahun)])
+
+def show_latest_data():
+  print(df.tail(20))
+
+def update_data(idx):
+  try:
+    if df.loc[idx].any():
+      print(df.loc[idx])
+      update_data = input('anda akan memperbarui data di atas? (y/n) : ')
+      if update_data == 'y' or update_data == 'Y':
+        input_data = True
+        print('Anda akan memperbarui data di atas')
+        print('''\nlist produk :
+        (0) : arabika toraja
+        (1) : arabika blue ijen
+        (2) : arabika gayo
+        (3) : arabika kintamani
+        (4) : robusta bogor
+        (5) : robusta gayo
+        (6) : arabika papua wamena
+        (7) : robusta dampit''')
+        print('\n~ Masukan berupa angka dari kode produk')
+        while input_data == True:
+          try:
+            produk = input('masukkan kode produk : ')
+            harga = int(input('masukkan harga : '))
+            bulan = input('masukkan bulan : ')
+            tahun = input('masukkan tahun : ')
+            kode_produk = '01234567'
+            if produk in kode_produk:
+              print(f'''data baru :
+              bulan = {bulan} 
+              tahun = {tahun}
+              kode produk = {produk}
+              harga = {harga}''')
+              print('pastikan kembali data yang baru sudah benar!')
+              input_data = input('apakah data yang akan anda tambahkan sudah benar? (y/n) : ')
+              if input_data == 'y' or input_data == 'Y':
+                df.loc[idx] = [bulan, tahun, prod_dict[produk], harga]
+                df.to_csv('bijikopi.csv', index=False)
+                input_data = False
+              else:
+                input_data = True
+            else:
+              print('kode produk tidak sesuai!')
+              input_data = False
+          except:
+            print('Error! harga harus berupa angka')
+      else:
+          print('Anda akan ditujukan keluar dari program')
+  except:
+    print('data tidak ditemukan')
+    
+def del_data(idx):
+  global df
+  try:
+    if df.loc[idx].any():
+      print(df.loc[idx])
+      delete_data = input('anda akan menghapus data di atas? (y/n) : ')
+      if delete_data == 'y' or delete_data == 'Y':    
+          df = df.drop(idx)
+          print('Data berhasil dihapus!')
+          print('Anda akan ditujukan keluar dari program untuk merefresh data penjualan terkini')
+          df.to_csv('bijikopi.csv', index=False)
+      else:
+          print('Anda akan ditujukan keluar dari program')
+    
+  except:
+    print('data tidak ditemukan')
--- menu.py
+++ menu.py
@@ -1,4 +1,4 @@
-from laporan import show_laporan
+from laporan import show_laporan, add_data, show_data, show_latest_data, update_data, del_data
 #pip install pwinput
 from pwinput import pwinput
 
@@ -11,7 +11,7 @@ for i in range(3):
     login_passw = pwinput('masukkan password : ')
     #login_passw = str(input('masukkan password : '))
     if (login_username == username) and (login_passw == passw):
-        print('login berhasil!\n')
+        print('login berhasil!')
         login = True
         break
     else:
@@ -19,7 +19,7 @@ for i in range(3):
     if i == 2:
         print('Anda gagal login ke sistem, silahkan jalankan ulang program')
 while login == True:
-    print('='*10, 'SELAMAT DATANG DI SISTEM PENCATATAN BIJI KOPI', '='*10)
+    print('\n','='*10, 'SELAMAT DATANG DI SISTEM PENCATATAN BIJI KOPI', '='*10)
     print('''
           1. Laporan Penjualan
           2. Tambah Data
@@ -33,5 +33,93 @@ while login == True:
         tahun = input('masukkan tahun : ')
         show_laporan(bulan, tahun)
         
+    elif inputan == '2':
+        input_data = True
+        print('Anda akan menambahkan data baru')
+        print('''\nlist produk :
+        (0) : arabika toraja
+        (1) : arabika blue ijen
+        (2) : arabika gayo
+        (3) : arabika kintamani
+        (4) : robusta bogor
+        (5) : robusta gayo
+        (6) : arabika papua wamena
+        (7) : robusta dampit''')
+        print('\n~ Masukan berupa angka dari kode produk')
+        while input_data == True:
+            try:
+              produk = input('masukkan kode produk : ')
+              harga = int(input('masukkan harga : '))
+              bulan = input('masukkan bulan : ')
+              tahun = input('masukkan tahun : ')
+              kode_produk = ['0','1','2','3','4','5','6','7']
+              if produk in kode_produk:
+                print(f'''data baru :
+    bulan = {bulan} 
+    tahun = {tahun}
+    kode produk = {produk}
+    harga = {harga}''')
+                print('pastikan kembali data baru sudah benar!')
+                input_data = input('apakah data yang akan anda tambahkan sudah benar? (y/n) : ')
+                if input_data == 'y' or input_data == 'Y':
+                  add_data(bulan, tahun, produk, harga)
+                  print('Data berhasil ditambahkan!')
+                  print('Anda akan ditujukan keluar dari program untuk merefresh data penjualan terkini')
+                  input_data = False
+                  login = False
+                else:
+                  input_data = True
+              else:
+                print('kode produk tidak sesuai!')
+                input_data = False
+            except:
+                print('harga harus berupa angka!')
+
+    elif inputan == '3':
+        print('''Anda akan mengelola data
+        1. Lihat data
+        2. Perbarui data
+        3. Hapus data''')
+        inputan_kelola = input('masukkan pilihan : ')
+        if inputan_kelola == '1':
+            print('1. lihat data terbaru \n2. lihat data berdasarkan bulan dan tahun')
+            inputan_kelola = input('masukkan pilihan : ')
+            if inputan_kelola == '1':
+                print('Anda akan melihat data penjualan terbaru')
+                show_latest_data()
+            elif inputan_kelola == '2':
+                print('Anda akan melihat data penjualan berdasar bulan dan tahun')
+                bulan = input('masukkan bulan : ')
+                tahun = input('masukkan tahun : ')
+                show_data(bulan, tahun)
+        elif inputan_kelola == '2':
+            index = True
+            print('Anda akan memperbarui data')
+            while index == True:
+                try:
+                    idx = int(input('masukkan index dari data : '))
+                    update_data(idx)
+                    index = False
+                    login = False
+                except:
+                    print('index harus berupa angka')
+        elif inputan_kelola == '3':
+            index = True
+            print('Anda akan menghapus data')
+            print("ketik '-' untuk kembali")
+            while index == True:
+                try:
+                    idx = str(input('masukkan index dari data : '))
+                    if idx == '-':
+                        index = False
+                    else:
+                        del_data(int(idx))
+                        index = False
+                        login = False
+                except:
+                    print('index harus berupa angka')
+                
     elif inputan == '4':
         login = False
+    else:
+        print('Inputan tidak sesuai!')
